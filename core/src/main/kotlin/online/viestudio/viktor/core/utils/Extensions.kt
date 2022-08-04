92f27dca122a7bafbd5b6bab5b75f6150ddf8970
Viktor Hrekh <hrekhviktor@gmail.com>
2022-08-04 17:59:31 +0300

    Remove redundant receiver and rename Desktop.browse utility extension.

--- core/src/main/kotlin/online/viestudio/viktor/core/utils/Extensions.kt
+++ core/src/main/kotlin/online/viestudio/viktor/core/utils/Extensions.kt
@@ -3,4 +3,4 @@ package online.viestudio.viktor.core.utils
 import java.awt.Desktop
 import java.net.URI
 
-fun Desktop.browse(uri: String) = Desktop.getDesktop().browse(URI.create(uri))
+fun browseUri(uri: String) = Desktop.getDesktop().browse(URI.create(uri))
