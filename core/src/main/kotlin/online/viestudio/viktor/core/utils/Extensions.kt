02eaffde88bc5128e7eef3fa786111f0d7ac8318
Viktor Hrekh <hrekhviktor@gmail.com>
2022-08-04 18:02:53 +0300

    Add logging utility extensions

--- core/src/main/kotlin/online/viestudio/viktor/core/utils/Extensions.kt
+++ core/src/main/kotlin/online/viestudio/viktor/core/utils/Extensions.kt
@@ -1,6 +1,15 @@
 package online.viestudio.viktor.core.utils
 
+import mu.KLogger
 import java.awt.Desktop
 import java.net.URI
 
 fun browseUri(uri: String) = Desktop.getDesktop().browse(URI.create(uri))
+
+fun KLogger.measuredInfo(action: String, millis: Long) = info { "$action in $millis ms." }
+
+fun KLogger.measuredDebug(action: String, millis: Long) = debug { "$action in $millis ms." }
+
+fun KLogger.failedError(action: String, throwable: Throwable) = error(throwable) { "$action failed." }
+
+fun KLogger.failedWarn(action: String, throwable: Throwable) = warn(throwable) { "$action failed." }
